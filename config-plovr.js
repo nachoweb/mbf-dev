cb1be9f66123a9080c0d83be52d6ded07c13a7dd
Nachoweb <nachoelg@gmail.com>
2012-05-01 09:17:27 +0200

    Cambios en config-plovr.js: 	Incluyo output-file para generar con plovr build el archivo salida 	Incluyo define goog.DEBUG para no incluir el codigo de debug en produccion

--- config-plovr.js
+++ config-plovr.js
@@ -2,11 +2,18 @@
   // The id is used as a query parameter in the src of the <script> tag.
   "id": "mbf",
   "paths": ".",
-  "inputs": "js/app.js", //Bootstrap de la aplicación
+  //Bootstrap de la aplicación
+  "inputs": "js/app.js",
+  //En produccion pondremos mode ADVANCED
   "mode":"SIMPLE",
   "level":"VERBOSE",
   "css-inputs":"gss/style.gss",
   "css-output-file":"gss/build/buildcss.css",
-	"externs":"externs.js"
+	"output-file":"js/build/buildapp.js",
+	"externs":"externs.js",
+	"define":{
+		//Lo pondremos a false cuando queramos generar el archivo de producción para quitar todo el codigo que vaya en un if(goog.DEBUG)
+		"goog.DEBUG":true  
+	}
   
 }
