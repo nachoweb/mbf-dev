9cb82c3760e26a01651909fbe49e9e5391e9c7e8
Nacho Navarro <neosorro@gmail.com>
2012-08-05 10:13:37 +0200

    CrazyEgg code

--- application/views/footer.php
+++ application/views/footer.php
@@ -25,6 +25,15 @@
         });
     </script>  
     <!-- End olark code -->
+    <!-- CrazyEgg -->
+    <script type="text/javascript">
+        setTimeout(function(){var a=document.createElement("script");
+        var b=document.getElementsByTagName("script")[0];
+        a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0013/1244.js?"+Math.floor(new Date().getTime()/3600000);
+        a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+    </script>
+    <!-- End CrayEgg -->
+
 </body>
     
     
