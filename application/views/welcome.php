a476173219eae2976d22012e5fae9a867f033956
Nacho Navarro <neosorro@gmail.com>
2012-10-08 20:26:56 +0200

    facebook connect

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -26,7 +26,7 @@
                             if (response.authResponse) {
                                 console.log('Welcome!  Fetching your information.... ');
                                 FB.api('/me', function(response) {
-                                console.log('Good to see you, ' + response.name + '.');
+                                console.log(response);
                                 });
                             } else {
                                 console.log('User cancelled login or did not fully authorize.');
