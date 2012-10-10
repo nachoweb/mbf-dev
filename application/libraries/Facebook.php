56a6037995e89158fd015b7b5dcaa178d5488510
Nacho Navarro <neosorro@gmail.com>
2012-10-10 21:20:07 +0200

    asd

--- application/libraries/Facebook.php
+++ application/libraries/Facebook.php
@@ -90,9 +90,6 @@ class FacebookApiException extends Exception
   }
 }
 
-?>
-
-<?php
 /**
  * Extends the BaseFacebook class with the intent of using
  * PHP sessions to store user ids and access tokens.
